63047a9329b47268654dffd5d0d4ac3884d983e7
VIK2395 <vik.chupak@gmail.com>
2021-03-01 18:57:14 +0200

    next try to deploy

--- app.js
+++ app.js
@@ -5,24 +5,22 @@ const path = require("path");
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "client", "build")));
+
 app.use(express.json({ extended: true }));
 
 app.use("/auth", require("./routs/auth"));
 app.use("/todo", require("./routs/todo"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  })
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+})
 
 const PORT = process.env.PORT || 5000;
 
 async function start() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || config.get("mongoUri"), {
+    await mongoose.connect(config.get("mongoUri"), {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
